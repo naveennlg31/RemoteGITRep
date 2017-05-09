5db2cf568318b9efd9a6c0ef5280f2841624d230
naveen <naveen@gmail.com>
2017-05-09 16:20:06 +0530

    Commit 2 on Proj2.. from system no:25 in karthik i tech lab

--- Proj2/src/com/nit/test/DemoTest2.java
+++ Proj2/src/com/nit/test/DemoTest2.java
@@ -3,7 +3,10 @@ package com.nit.test;
 public class DemoTest2 {
 
 	public static void main(String[] args) {
+		int a=2;int b=3;
 		System.out.println("hello world 1");
 		System.out.println("hello world 2");
+		System.out.println("sum of a and b is:");
+		System.out.println("--------------------");
 	}
 }
